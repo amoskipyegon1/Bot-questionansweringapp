2c1d10aca763488fbc75e331069dec1f95a6d618
Amos-Ditto <amoskip@kabarak.ac.ke>
2021-10-17 12:44:27 +0300

    editing this

--- Chat2/settings.py
+++ Chat2/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import django_heroku
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&r5d8$p0zrnpkygq=zxj1ts)k^j2scm_p+teg741aa@=z28v!n'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'chat',
         'USER': 'amoskipyegon',
-        'PASSWORD': 'amos2001',
+        'PASSWORD': config('PASSWORD'),
         'HOST': 'localhost',
         'PORT': '',
     }
